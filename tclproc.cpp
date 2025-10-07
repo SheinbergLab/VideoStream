18c5280bc5bd245e9bb23801144858e1e8f46b20
David Sheinberg <David_Sheinberg@brown.edu>
2025-10-07 11:54:43 -0400

    moved tcl code string from tclcode.cpp

--- tclproc.cpp
+++ tclproc.cpp
@@ -407,50 +407,63 @@ void addTclCommands(Tcl_Interp *interp, proginfo_t *p)
   Tcl_LinkVar(interp, "vstream::displayEvery", 
           (char *) &useWebcam, TCL_LINK_INT);
 
-#if 0
-  const char *ds_str = R"V0G0N(
+const char *ds_str = R"V0G0N(
   proc vstream::dsRegister { server { port 4620 } } {
-    set s [socket $server $port]
-    fconfigure $s -buffering line
-    set our_ip [lindex [fconfigure $s -sockname] 0]
-    puts $s "%reg $our_ip $::vstream::dsPort"
-    catch {gets $s status}
-    close $s
-    return $status
+	set s [socket $server $port]
+	fconfigure $s -buffering line
+	set our_ip [lindex [fconfigure $s -sockname] 0]
+	puts $s "%reg $our_ip $::vstream::dsPort"
+	catch {gets $s status}
+	close $s
+        if { $status } { 
+           set ::vstream::dsServerIP $server 
+           set ::vstream::dsServerPort $port
+        }
+	return $status
     }
 
-    proc vstream::dsUnregister { server { port 4620 } } {
-    set s [socket $server $port]
-    fconfigure $s -buffering line
-    set our_ip [lindex [fconfigure $s -sockname] 0]
-    puts $s "%unreg $our_ip $::vstream::dsPort"
-    catch {gets $s status}
-    close $s
-    return $status
+    proc vstream::dsUnregister { } {
+        if { ![info exists ::vstream::dsServerIP] } { 
+             return 0 
+        } else {
+             set server $::vstream::dsServerIP
+        }
+        if { ![info exists ::vstream::dsServerPort] } { 
+             return 0 
+        } else {
+             set port $::vstream::dsServerPort
+        }
+	set s [socket $server $port]
+	fconfigure $s -buffering line
+	set our_ip [lindex [fconfigure $s -sockname] 0]
+	puts $s "%unreg $our_ip $::vstream::dsPort"
+	catch {gets $s status}
+	close $s
+        unset ::vstream::dsServerIP ::vstream::dsServerPort
+	return $status
     }
 
     proc vstream::dsAddMatch { server pattern { port 4620 } } {
-    set s [socket $server $port]
-    fconfigure $s -buffering line
-    set our_ip [lindex [fconfigure $s -sockname] 0]
-    puts $s "%match $our_ip $::vstream::dsPort $pattern 1"
-    catch {gets $s status}
-    close $s
-    return $status
+	set s [socket $server $port]
+	fconfigure $s -buffering line
+	set our_ip [lindex [fconfigure $s -sockname] 0]
+	puts $s "%match $our_ip $::vstream::dsPort $pattern 1"
+	catch {gets $s status}
+	close $s
+	return $status
     }
 
     proc vstream::dsRemoveMatch { server pattern { port 4620 } } {
-    set s [socket $server $port]
-    fconfigure $s -buffering line
-    set our_ip [lindex [fconfigure $s -sockname] 0]
-    puts $s "%unmatch $our_ip $::vstream::dsPort $pattern"
-    catch {gets $s status}
-    close $s
-    return $status
+	set s [socket $server $port]
+	fconfigure $s -buffering line
+	set our_ip [lindex [fconfigure $s -sockname] 0]
+	puts $s "%unmatch $our_ip $::vstream::dsPort $pattern"
+	catch {gets $s status}
+	close $s
+	return $status
     }
 )V0G0N";
-#endif
-  extern const char *ds_str;
+
   Tcl_Eval(interp, ds_str);
 
   Tcl_LinkVar(interp, "vstream::show_chunk",
