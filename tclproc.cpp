72c238a16bbd9263d7859ede51642219081f64a8
David Sheinberg <David_Sheinberg@brown.edu>
2025-10-08 17:26:29 -0400

    added source and review commands

--- tclproc.cpp
+++ tclproc.cpp
@@ -20,9 +20,12 @@
 #include <math.h>
 #include <tcl.h>
 
-#include "VideoStream.h"
 #include "IFrameSource.h"
+#include "SourceManager.h"
 #include "FlirCameraSource.h"
+#include "ReviewModeSource.h"
+#include "FrameBufferManager.h"
+#include "VideoStream.h"
 
 #ifdef WIN32
 static int strcasecmp(char *a,char *b) { return stricmp(a,b); }
@@ -49,6 +52,244 @@ static int pingCmd(ClientData clientData, Tcl_Interp *interp,
   if (argc > 1) {
     Tcl_AppendResult(interp, " ", argv[1], NULL);
   }
+  return TCL_OK;
+}
+
+
+/*********************************************************************/
+/*                         Source Commands                           */
+/*********************************************************************/
+
+
+static int startSourceCmd(ClientData clientData, Tcl_Interp *interp,
+                         int objc, Tcl_Obj *const objv[]) {
+    proginfo_t *p = (proginfo_t *)clientData;
+    
+    if (objc < 2) {
+        Tcl_WrongNumArgs(interp, 1, objv, "type ?key value ...?");
+        return TCL_ERROR;
+    }
+    
+    std::string type = Tcl_GetString(objv[1]);
+    std::map<std::string, std::string> params;
+    
+    // Parse key-value pairs
+    for (int i = 2; i + 1 < objc; i += 2) {
+        std::string key = Tcl_GetString(objv[i]);
+        std::string val = Tcl_GetString(objv[i+1]);
+        params[key] = val;
+    }
+    
+    // Access through proginfo_t
+    SourceManager* sm = p->sourceManager;
+    IFrameSource** fs = p->frameSource;
+    
+    // Stop existing source first if running
+    if (sm->getState() == SOURCE_RUNNING) {
+        std::cout << "Stopping existing source before starting new one..." << std::endl;
+        sm->stopSource();
+        *fs = nullptr;
+        // Small delay to ensure clean shutdown
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+    
+    // Start new source
+    if (sm->startSource(type, params)) {
+        *fs = sm->getCurrentSource();
+        
+        // Update global properties through proginfo_t pointers
+        if (*fs) {
+            *(p->frame_rate) = (*fs)->getFrameRate();
+            *(p->frame_width) = (*fs)->getWidth();
+            *(p->frame_height) = (*fs)->getHeight();
+            *(p->is_color) = (*fs)->isColor();
+            
+            std::cout << "New source: " << *(p->frame_width) << "x" << *(p->frame_height) 
+                      << " @ " << *(p->frame_rate) << " fps" << std::endl;
+        }
+        
+        Tcl_SetObjResult(interp, Tcl_NewStringObj("ok", -1));
+        return TCL_OK;
+    } else {
+        Tcl_SetObjResult(interp, Tcl_NewStringObj("failed to start source", -1));
+        return TCL_ERROR;
+    }
+}
+
+static int stopSourceCmd(ClientData clientData, Tcl_Interp *interp,
+                        int objc, Tcl_Obj *const objv[]) {
+    proginfo_t *p = (proginfo_t *)clientData;
+    
+    SourceManager* sm = p->sourceManager;
+    IFrameSource** fs = p->frameSource;
+    
+    if (sm->stopSource()) {
+        *fs = nullptr;
+        Tcl_SetObjResult(interp, Tcl_NewStringObj("ok", -1));
+        return TCL_OK;
+    } else {
+        Tcl_SetObjResult(interp, Tcl_NewStringObj("no active source or already stopped", -1));
+        return TCL_ERROR;
+    }
+}
+
+static int getSourceStatusCmd(ClientData clientData, Tcl_Interp *interp,
+                             int objc, Tcl_Obj *const objv[]) {
+    proginfo_t *p = (proginfo_t *)clientData;
+    
+    SourceManager* sm = p->sourceManager;
+    IFrameSource** fs = p->frameSource;
+    
+    Tcl_Obj* statusDict = Tcl_NewDictObj();
+    
+    // State
+    std::string state_str;
+    switch (sm->getState()) {
+        case SOURCE_IDLE: state_str = "idle"; break;
+        case SOURCE_RUNNING: state_str = "running"; break;
+        case SOURCE_PAUSED: state_str = "paused"; break;
+        case SOURCE_STOPPING: state_str = "stopping"; break;
+        case SOURCE_ERROR: state_str = "error"; break;
+        default: state_str = "unknown"; break;
+    }
+    
+    Tcl_DictObjPut(interp, statusDict, Tcl_NewStringObj("state", -1),
+                  Tcl_NewStringObj(state_str.c_str(), -1));
+    
+    // Type
+    std::string type = sm->getSourceType();
+    Tcl_DictObjPut(interp, statusDict, Tcl_NewStringObj("type", -1),
+                  Tcl_NewStringObj(type.c_str(), -1));
+    
+    // Frame properties (if source is active)
+    if (*fs) {
+        Tcl_DictObjPut(interp, statusDict, Tcl_NewStringObj("width", -1),
+                      Tcl_NewIntObj(*(p->frame_width)));
+        Tcl_DictObjPut(interp, statusDict, Tcl_NewStringObj("height", -1),
+                      Tcl_NewIntObj(*(p->frame_height)));
+        Tcl_DictObjPut(interp, statusDict, Tcl_NewStringObj("fps", -1),
+                      Tcl_NewDoubleObj(*(p->frame_rate)));
+        Tcl_DictObjPut(interp, statusDict, Tcl_NewStringObj("color", -1),
+                      Tcl_NewBooleanObj(*(p->is_color)));
+    }
+    
+    Tcl_SetObjResult(interp, statusDict);
+    return TCL_OK;
+}
+
+/*********************************************************************/
+/*                         Review Commands                           */
+/*********************************************************************/
+
+int reviewClearCmd(ClientData data, Tcl_Interp *interp, 
+                    int objc, Tcl_Obj *const objv[])
+{
+  proginfo_t *p = (proginfo_t *)data;
+  p->sourceManager->getReviewSource()->clearFrames();
+  return TCL_OK;
+}
+
+int reviewSampleCmd(ClientData data, Tcl_Interp *interp,
+		 int objc, Tcl_Obj *const objv[])
+{
+  proginfo_t *p = (proginfo_t *)data;
+  
+  cv::Mat frame_copy;
+  FrameMetadata metadata;
+  bool in_obs;
+  
+  // Thread-safe copy from the frame buffer
+  if (p->frameBuffer->copyFrame(*(p->displayFrame), frame_copy, metadata, in_obs)) {
+    // Add to review source
+    if (p->sourceManager->sampleCurrentFrame(frame_copy, metadata)) {
+      Tcl_SetObjResult(interp, Tcl_NewIntObj(
+					     p->sourceManager->getReviewSource()->getFrameCount()));
+      return TCL_OK;
+    }
+  }
+  
+  Tcl_SetObjResult(interp, Tcl_NewIntObj(0));
+  return TCL_OK;
+}
+
+int reviewRemoveCmd(ClientData data, Tcl_Interp *interp,
+                     int objc, Tcl_Obj *const objv[])
+{
+  proginfo_t *p = (proginfo_t *)data;
+  if (!p->reviewSource) return TCL_OK;
+  if (objc != 2) {
+    Tcl_WrongNumArgs(interp, 1, objv, "index");
+    return TCL_ERROR;
+  }
+  
+  int index;
+  if (Tcl_GetIntFromObj(interp, objv[1], &index) != TCL_OK) {
+    return TCL_ERROR;
+  }
+  p->sourceManager->getReviewSource()->removeFrame(index);
+
+  return TCL_OK;
+}
+
+int reviewNextCmd(ClientData data, Tcl_Interp *interp,
+		  int objc, Tcl_Obj *const objv[])
+{
+  proginfo_t *p = (proginfo_t *)data;
+  p->sourceManager->getReviewSource()->nextFrame();
+  return TCL_OK;
+}
+
+int reviewPreviousCmd(ClientData data, Tcl_Interp *interp,
+                       int objc, Tcl_Obj *const objv[])
+{
+  proginfo_t *p = (proginfo_t *)data;
+  p->sourceManager->getReviewSource()->previousFrame();
+  return TCL_OK;
+}
+
+int reviewJumpToCmd(ClientData data, Tcl_Interp *interp,
+		    int objc, Tcl_Obj *const objv[])
+{
+  proginfo_t *p = (proginfo_t *)data;
+  if (objc != 2) {
+    Tcl_WrongNumArgs(interp, 1, objv, "index");
+    return TCL_ERROR;
+  }
+  
+  int index;
+  if (Tcl_GetIntFromObj(interp, objv[1], &index) != TCL_OK) {
+    return TCL_ERROR;
+  }
+
+  if (index < 0 ||
+      index >= p->sourceManager->getReviewSource()->getFrameCount()) {
+    Tcl_AppendResult(interp, Tcl_GetString(objv[0]),
+		     ":index out of range", NULL);
+    return TCL_ERROR;
+  }
+  
+  p->sourceManager->getReviewSource()->jumpToFrame(index);
+
+  return TCL_OK;
+}
+
+int reviewCountCmd(ClientData data, Tcl_Interp *interp,
+                    int objc, Tcl_Obj *const objv[])
+{
+  proginfo_t *p = (proginfo_t *)data;
+
+  int count = 0;
+  Tcl_SetObjResult(interp,
+		   Tcl_NewIntObj(p->sourceManager->getReviewSource()->getFrameCount()));
+  return TCL_OK;
+}
+
+int reviewIndexCmd(ClientData data, Tcl_Interp *interp,
+                    int objc, Tcl_Obj *const objv[])
+{
+  proginfo_t *p = (proginfo_t *)data;
+  Tcl_SetObjResult(interp,
+		   Tcl_NewIntObj(p->sourceManager->getReviewSource()->getCurrentIndex()));
   return TCL_OK;
 }
 
@@ -346,6 +587,29 @@ void addTclCommands(Tcl_Interp *interp, proginfo_t *p)
             (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL);
 
   Tcl_Eval(interp, "namespace eval vstream {}");
+
+  Tcl_CreateObjCommand(interp, "::vstream::startSource", startSourceCmd, p, NULL);
+  Tcl_CreateObjCommand(interp, "::vstream::stopSource", stopSourceCmd, p, NULL);
+  Tcl_CreateObjCommand(interp, "::vstream::getSourceStatus", getSourceStatusCmd, p, NULL);
+
+  Tcl_CreateObjCommand(interp, "::vstream::reviewClear", reviewClearCmd, 
+		       (ClientData)p, (Tcl_CmdDeleteProc *)NULL);
+  Tcl_CreateObjCommand(interp, "::vstream::reviewSample", reviewSampleCmd,
+		       (ClientData)p, (Tcl_CmdDeleteProc *)NULL);
+  Tcl_CreateObjCommand(interp, "::vstream::reviewRemove", reviewRemoveCmd,
+		       (ClientData)p, (Tcl_CmdDeleteProc *)NULL);
+  Tcl_CreateObjCommand(interp, "::vstream::reviewNext", reviewNextCmd,
+		       (ClientData)p, (Tcl_CmdDeleteProc *)NULL);
+  Tcl_CreateObjCommand(interp, "::vstream::reviewPrevious", reviewPreviousCmd,
+		       (ClientData)p, (Tcl_CmdDeleteProc *)NULL);
+  Tcl_CreateObjCommand(interp, "::vstream::reviewJumpTo", reviewJumpToCmd,
+		       (ClientData)p, (Tcl_CmdDeleteProc *)NULL);
+  Tcl_CreateObjCommand(interp, "::vstream::reviewCount", reviewCountCmd,
+		       (ClientData)p, (Tcl_CmdDeleteProc *)NULL);
+  Tcl_CreateObjCommand(interp, "::vstream::reviewIndex", reviewIndexCmd,
+		       (ClientData)p, (Tcl_CmdDeleteProc *)NULL);
+
+  
   Tcl_CreateCommand(interp, "vstream::fileOpen", (Tcl_CmdProc *) openFileCmd, 
             (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL);
   Tcl_CreateCommand(interp, "vstream::fileClose",
@@ -400,12 +664,9 @@ void addTclCommands(Tcl_Interp *interp, proginfo_t *p)
   extern int dsPort;
   Tcl_LinkVar(interp, "vstream::dsPort", (char *) &dsPort, TCL_LINK_INT);
 
-  extern int useWebcam;
-  Tcl_LinkVar(interp, "vstream::useWebcam", (char *) &useWebcam, TCL_LINK_INT);
-
   extern int displayEvery;
   Tcl_LinkVar(interp, "vstream::displayEvery", 
-          (char *) &useWebcam, TCL_LINK_INT);
+          (char *) &displayEvery, TCL_LINK_INT);
 
 const char *ds_str = R"V0G0N(
   proc vstream::dsRegister { server { port 4620 } } {
