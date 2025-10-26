b88516791f8b159e2df745c9aa3b084973647fd1
David Sheinberg <David_Sheinberg@brown.edu>
2025-10-26 16:21:53 -0400

    initial commit

---
+++ VstreamVars.h
@@ -0,0 +1,8 @@
+/* Shared with tcl */
+extern int dsPort;
+extern int useWebcam;
+extern int displayEvery;   // Determines how often to update display
+
+extern int ShowChunk;
+extern bool in_obs;
+extern bool ds_in_obs;
